11c2512abba37e16f2f1cc39c3c349be63ae33bb
Laiba Malik <laibakk.091@gmail.com>
2024-10-28 12:47:15 -0700

    Add files via upload

---
+++ Scripe.js
@@ -0,0 +1,7 @@
+// guess the Number Game
+let gameNo=10;
+userNo=prompt("Guess the right No!");
+while(userNo!=gameNo){
+    userNo=prompt("Try again!")
+}
+console.log(userNo,"congratulation you guess the right no!")
